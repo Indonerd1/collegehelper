c3745999f14196216f255e7bc7c3bb80b583a13b
Indonerd1 <70823513+Indonerd1@users.noreply.github.com>
2020-09-06 13:55:51 -0400

    Add files via upload

---
+++ helper.java
@@ -0,0 +1,278 @@
+package Helper;
+
+import java.util.Scanner;
+
+	public class helper {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner (System.in);
+		
+	System.out.println("Insert GPA UNDER THE QUESTION(sorry only 200 works"
+			+ ". the software cannot hold that much info but if it was on a different one it would work 100%");
+	int  x=sc.nextInt();
+	if (x==200)  {
+
+			System.out.println("You can apply for: "
+					+ "Columbia College"
+					+ "-Inter American University of Puerto Rico - Fajardo Campus"
+					+ "-Voorhees College");
+	if (x==201)  {
+
+		System.out.println("You can apply for:"
+				+ "-Concordia University Ann Arbor\r\n" + 
+				"-Dean College\r\n" + 
+				"-Shaw University\r\n" + 
+				"Saint Augustines College\r\n" + 
+				"-Catawba College\r\n" + 
+				"-Webber International University\r\n" + 
+				"-Fisher College\r\n" + 
+				"");
+		if (x==202)  {
+
+			System.out.println("You can apply for: "
+					+ "-Kentucky State University\r\n" + 
+					"-Le Moyne Owen College\r\n" + 
+					"-Morrison Institute of Technology"
+					+ "-Maria College of Albany"
+					+ "-Polytechnic University of Puerto Rico-Miam"
+					+ "-BridgeValley Community & Technical College\r\n");
+			if (x==203)  {
+
+	System.out.println("You can apply for: "
+						+ "-Bowie State University"
+						+ "-Fresno City College"
+						+ "-Fisher College"
+						+ "-Miles College"
+						+ "-Suffolk County Community College"
+						+ "-Livingstone College"
+						+ "-Newbury College"
+						+ "-Southern Vermont College");
+				if (x==204)  {
+
+				
+					System.out.println("You can apply for: "
+							+ "-Lincoln University"
+							+ "-Nassau Community College"
+							+ "-New England College"
+							+ "-Coppin State University"
+							+ "-Shaw University"
+							+ "-Suffolk County Community College"
+							+ "-Wiley College"
+							+ "-Lane College"
+							+ "-Villa Maria College");
+					if (x==205)  {
+
+						System.out.println("You can apply for:"
+								+ "-Wesley College"
+								+ "-Virginia Union University"
+								+ "-Florida Memorial University"
+								+ "-Owens Community College"
+								+ "-National Louis University"
+								+ "-Chowan University"
+								+ "-Livingstone College"
+								+ "-Lane College"
+								+ "-University of South Carolina-Upstate");
+						if (x==206)  {
+
+							System.out.println("You can apply for:"
+									+ "-Southern Vermont College"
+									+ "-East-West University"
+									+ "-Villa Maria College"
+									+ "-Edward Waters College"
+									+ "-Chattanooga State Community College"
+									+ "-Fayetteville Technical Community College\r\n" + 
+									"-Albertus Magnus College");
+							
+							if (x==207)  {
+
+								System.out.println("You can apply for:"
+										+ "-Texas Southern University"
+										+ "-Savannah State University"
+										+ "-Grambling State University"
+										+ "-Northeastern Illinois University"
+										+ "-Coppin State University"
+										+ "-Dillard University"
+										+ "-Governors State University"
+										+ "-Nevada State College");
+								if (x==208)  {
+
+									System.out.println("You can apply for: "
+											+ "-Tennessee State University"
+											+ "-Texas Southern University"
+											+ "-University of Baltimore"
+											+ "-Albany State University"
+											+ "-Virginia State University\r\n" + 
+											"-Savannah State University"
+											+ "-Norfolk State University"
+											+ "-SUNY College at Brockport");
+									if (x==209)  {
+
+										System.out.println("You can apply for:"
+												+ "-Morgan State University"
+												+ "-Lynn University"
+												+ "-Kean University "
+												+ "-Berklee College of Music"
+												+ "-Alabama A & M University"
+												+ "-Albany State University"
+												+ "-Iona College"
+												+ "-Clayton State University"
+												+ "-University of Bridgeport");
+										if (x==300)  {
+
+						System.out.println("You can apply for: "
+								+ "-University of Hartford"
+								+ "-University of Wisconsin-Milwaukee"
+								+ "-The Evergreen State College"
+								+ "-Colorado Mesa University"
+								+ "-Tuskegee University"
+								+ "-Barry University"
+								+ "-Stevenson University"
+								+ "-North Park University"
+								+ "-Lincoln University"
+								+ "-University of Bridgeport");
+						if (x==301)  {
+
+							System.out.println("You can apply for:"
+									+ "-James Madison University"
+									+ "-Morgan State University"
+									+ "-California State University-Fresno"
+									+ "-St. Thomas University"
+									+ "-University of Hartford"
+									+ "-Suffolk University"
+									+ "-The Evergreen State College"
+									+ "-Southwestern College"
+									+ "University of West Georgia"
+									+ "-Miami Dade College"
+									+ "-Indiana State University"
+									+ "-Southern New Hampshire University");
+							if (x==302)  {
+
+								System.out.println("You can apply for:"
+										+ "-Howard University"
+										+ "-Sam Houston State University"
+										+ "-University of Illinois at Chicago"
+										+ "-San Francisco State University"
+										+ "-Radford University"
+										+ "-Clark Atlanta University"
+										+ "-Montclair State University"
+										+ "-Columbia College Chicago ");
+								if (x==303)  {
+
+									System.out.println("You can apply for: "
+											+ "-Central Michigan University"
+											+ "-Texas State University"
+											+ "-Louisiana State University and Agricultural & Mechanical College"
+											+ "-Pace University"
+											+ "-The University of Texas at San Antonio"
+											+ "-University of Nevada-Las Vegas"
+											+ "-Sam Houston State University"
+											+ "-Illinois State University"
+											+ "-University of Illinois at Chicago"
+											+ "-San Francisco State University");
+									if (x==304)  {
+
+										System.out.println("You can apply for:"
+												+ "-Arizona State University-Tempe"
+												+ "-University of Arizona"
+												+ "-Howard University"
+												+ "-Washington State University"
+												+ "-Central Michigan University"
+												+ "-Ohio University-Main Campus"
+												+ "-Temple University"
+												+ "-Georgia State University"
+												+ "-Liberty University"
+												+ "-University of Kentucky\r\n" + 
+												"");
+										if (x==305)  {
+
+											System.out.println("You can apply for:"
+													+ "-Pennsylvania State University-Main Campus"
+													+ "-New York University"
+													+ "University of Miami"
+													+ "University of Oregon"
+													+ "Washington State University"
+													+ "-University of Iowa"
+													+ "-University of Kentucky"
+													+ "-Pace University"
+													+ "-University of Cincinnati-Main Campus");
+											if (x==306)  {
+
+												System.out.println("You can apply for:"
+														+ "-Pennsylvania State University-Main Campus"
+														+ "-Boston University"
+														+ "-Purdue University-Main Campus"
+														+ "-New York University"
+														+ "-Michigan State University"
+														+ "-Syracuse University"
+														+ "-University of Illinois at Urbana-Champaign"
+														+ "-San Diego State University"
+														+ "-Indiana University-Bloomington");
+
+												if (x==307)  {
+
+													System.out.println("You can apply for: "
+															+ "-The University of Texas at Austin"
+															+ "-University of Southern California"
+															+ "-University of Washington-Seattle Campus"
+															+ "-Michigan State University"
+															+ "-Ohio State University-Main Campus"
+															+ "-Indiana University-Bloomington\r\n" + 
+															"-Baylor University"
+															+ "-University of Oregon");
+													if (x==308)  {
+
+														System.out.println("You can apply for:"
+																+ "-University of California-Los Angeles"
+																+ "Stanford University"
+																+ "-University of California-Berkeley"
+																+ "-University of Michigan-Ann Arbor"
+																+ "-The University of Texas at Austin"
+																+ "-University of Southern California"
+																+ "-Princeton University"
+																+ "-University of Pennsylvania");
+														if (x==309)  {
+
+															System.out.println("You can apply for:"
+																	+ "-University of California-Los Angeles"
+																	+ "University of Florida"
+																	+ "-University of Central Florida"
+																	+ "-Brown University"
+																	+ "-Northwestern University"
+																	+ "-Ohio State University-Main Campus"
+																	+ "-University of California-Davis"
+																	+ "-Georgia Institute of Technology-Main Campus"
+																	+ "-Florida State University");
+if       (x==400)  {
+System.out.println("You can apply for:"
++ "-University of California-Los Angeles"
++ "-Harvard College"
++ "-Georgetown University"
++ "-University of California-Irvine"
++ "-University of Maryland-College Park"
++ "-University of Houston");
+																
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	} 
+		
+	}
+	}
